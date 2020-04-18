dc2c9d276303af4a4ad45d067feccbf52c6a05d8
prathameshanabhavane <pratham291994@gmail.com>
2020-04-18 17:08:02 +0530

    string

--- helloworld/app.py
+++ helloworld/app.py
@@ -6,8 +6,17 @@ print(x)
 students_count = 1000
 rating = 4.99
 is_publihed = True
-course_name = 'Python progamming'
+course_name = 'Python programming'
 print(students_count)
 print(rating)
 print(is_publihed)
 print(course_name)
+
+
+print(len(course_name))
+print(course_name[0])
+print(course_name[-1])
+print(course_name[0:3])
+print(course_name[0:])
+print(course_name[:3])
+print(course_name[:])
