8791d8a884ac57eb8c5f7ec83e357cc5553f8cf6
prathameshanabhavane <pratham291994@gmail.com>
2020-04-18 18:42:14 +0530

    logical operator

--- helloworld/app.py
+++ helloworld/app.py
@@ -120,3 +120,30 @@ age = 22
 message = "Eligible" if age >= 18 else "Not eligile"
 
 print(message)
+
+
+# Logical operator
+
+high_income = False
+good_credit = True
+student = True
+
+# if high_income and good_credit:
+#     print("Eligible")
+# else:
+#     print("Not eligible")
+
+# if high_income or good_credit:
+#     print("Eligible")
+# else:
+#     print("Not eligible")
+
+if not student:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+if ((high_income or good_credit) and not student):
+    print("Eligible")
+else:
+    print("Not eligible")
