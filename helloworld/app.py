25b6bca5a1ca01bad6a2f1f204260982a1328473
prathameshanabhavane <pratham291994@gmail.com>
2020-04-18 19:17:56 +0530

    nested loop

--- helloworld/app.py
+++ helloworld/app.py
@@ -196,3 +196,10 @@ for number in range(3):
         break
 else:
     print('Attempted 3 times and failed')
+
+
+# Nested loop
+
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
