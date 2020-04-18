64c5f7b9177185a09f2668e29be9e06903a135c9
prathameshanabhavane <pratham291994@gmail.com>
2020-04-18 16:58:13 +0530

    variable name

---
+++ helloworld/app.py
@@ -0,0 +1,13 @@
+print('Hello World')
+print('*' * 10)
+x = 5
+print(x)
+
+students_count = 1000
+rating = 4.99
+is_publihed = True
+course_name = 'Python progamming'
+print(students_count)
+print(rating)
+print(is_publihed)
+print(course_name)
