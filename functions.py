aadb9ec553a7e0f5bbb219a6d8f308878881235a
prathameshanabhavane <pratham291994@gmail.com>
2020-04-19 14:00:59 +0530

    keyword argument

--- functions.py
+++ functions.py
@@ -1,6 +1,37 @@
-def greet(first_name, last_name):
-    print(f"Hi {first_name} {last_name}")
-    print("welcome aboard")
+# def greet(first_name, last_name):
+#     print(f"Hi {first_name} {last_name}")
+#     print("welcome aboard")
 
 
-greet("Prathamesh", "Anabhavane")
+# greet("Prathamesh", "Anabhavane")
+
+# 1. perform task
+# 2. retrun value
+
+#  1. perform task
+def greet(name):
+    print(f"hi {name}")
+
+
+greet("prathamesh")
+
+
+# 2. retrun value
+# every function has default retunr value is none
+
+
+def greeting(name):
+    return f"hi {name}"
+
+
+name = greeting("abc")
+
+print(name)
+
+
+# keyword argument
+def increament(number, by):
+    return number + by
+
+
+print(increament(2, by=1))
