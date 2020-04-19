a28ed5ce62668d4d6696909c5c0b34b283231fff
prathameshanabhavane <pratham291994@gmail.com>
2020-04-19 17:41:08 +0530

    list

---
+++ data_structure.py
@@ -0,0 +1,19 @@
+letters = ["a", "b", "c", "d"]
+matrix = [[0, 1], [1, 2], [2, 3]]
+zeros = [0] * 5
+combined = zeros + letters
+numbers = list(range(0, 21))
+chars = list("Hello World")
+lengthChar = len(chars)
+lengthNumbers = len(numbers)
+
+
+print(letters)
+print(letters[0])
+print(matrix)
+print(zeros)
+print(combined)
+print(numbers)
+print(chars)
+print(lengthChar)
+print(lengthNumbers)
