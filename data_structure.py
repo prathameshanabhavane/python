a50bc6de2a0e4ebd90e6e5e477daf134306526f8
prathameshanabhavane <pratham291994@gmail.com>
2020-04-19 17:49:47 +0530

    accessing item

--- data_structure.py
+++ data_structure.py
@@ -1,19 +1,43 @@
-letters = ["a", "b", "c", "d"]
-matrix = [[0, 1], [1, 2], [2, 3]]
-zeros = [0] * 5
-combined = zeros + letters
-numbers = list(range(0, 21))
-chars = list("Hello World")
-lengthChar = len(chars)
-lengthNumbers = len(numbers)
+# letters = ["a", "b", "c", "d"]
+# matrix = [[0, 1], [1, 2], [2, 3]]
+# zeros = [0] * 5
+# combined = zeros + letters
+# numbers = list(range(0, 21))
+# chars = list("Hello World")
+# lengthChar = len(chars)
+# lengthNumbers = len(numbers)
 
+
+# print(letters)
+# print(letters[0])
+# print(matrix)
+# print(zeros)
+# print(combined)
+# print(numbers)
+# print(chars)
+# print(lengthChar)
+# print(lengthNumbers)
+
+
+# Accessing Items
+
+letters = ["a", "b", "c", "d"]
+
+letters[0] = "A"
 
 print(letters)
-print(letters[0])
-print(matrix)
-print(zeros)
-print(combined)
-print(numbers)
-print(chars)
-print(lengthChar)
-print(lengthNumbers)
+print(letters[0:3])
+print(letters[1:])
+print(letters[:])
+print(letters[::2])
+print(letters[::-1])
+print(letters[-1])
+
+
+numbers = list(range(0, 20))
+
+print(numbers[::2])
+print(numbers[::3])
+print(numbers[::-3])
+print(numbers[::-2])
+print(numbers[::-1])
