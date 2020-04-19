855f3d6cd9b1e65bc6bada64ffe3c3554e54dec0
prathameshanabhavane <pratham291994@gmail.com>
2020-04-19 13:48:38 +0530

    arguments

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
-def greet():
-    print("Hi there")
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
     print("welcome aboard")
 
 
-greet()
+greet("Prathamesh", "Anabhavane")
