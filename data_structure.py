d2542ca8d18ea7e6f5a16ec2ea4cda416b8db143
prathameshanabhavane <pratham291994@gmail.com>
2020-04-19 19:22:00 +0530

    stacks

--- data_structure.py
+++ data_structure.py
@@ -172,3 +172,18 @@ list2 = [10, 20, 30]
 
 print(list(zip(list1, list2)))
 print(list(zip("abc", list1, list2)))
+
+# Stacks
+
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+
+
+last = browsing_session.pop()
+print(last)
+print(browsing_session)
+print("redirect", browsing_session[-1])
+if not browsing_session:
+    print("disable")
