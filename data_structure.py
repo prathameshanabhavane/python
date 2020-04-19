763b08abefc3b8f4d1afa3b55e41406dd3632f5c
prathameshanabhavane <pratham291994@gmail.com>
2020-04-19 20:31:22 +0530

    dictionary

--- data_structure.py
+++ data_structure.py
@@ -262,3 +262,22 @@ print(first ^ second)
 
 if 1 in first:
     print("Yes")
+
+
+# Dictionary
+
+# point = {"x": 1, "y": 2}
+point = dict(x=1, y=2)  # another method of create dictionary
+point["z"] = 20
+print(point)
+print(point["x"])
+del point["x"]
+print(point)
+
+if "a" in point:
+    print(point["a"])
+
+print(point.get("a", 0))
+
+for key, value in point.items():
+    print(key, value)
