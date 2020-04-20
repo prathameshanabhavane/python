a365cbfc19ecf87bb44aaa355092d3dcbeff4601
prathameshanabhavane <pratham291994@gmail.com>
2020-04-20 18:46:36 +0530

    handle different exception

--- exceptions.py
+++ exceptions.py
@@ -1,9 +1,21 @@
+# try:
+#     age = int(input("Age :"))
+# except ValueError as ex:
+#     print("You didn't enter a valid age")
+#     print(ex)
+#     print(type(ex))
+# else:
+#     print("NO exceptions were thrown")
+# print("Excution continues...")
+
+
+# Handling different exceptions
+
 try:
     age = int(input("Age :"))
-except ValueError as ex:
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError):
     print("You didn't enter a valid age")
-    print(ex)
-    print(type(ex))
 else:
     print("NO exceptions were thrown")
 print("Excution continues...")
