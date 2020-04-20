77f2d6e40d8883769781a8c764b9bef33663edce
prathameshanabhavane <pratham291994@gmail.com>
2020-04-20 19:21:48 +0530

    cost of raising exception

--- exceptions.py
+++ exceptions.py
@@ -47,6 +47,23 @@
 
 # Raising exception
 
+# def calculate_xfactor(age):
+#     if age <= 0:
+#         raise ValueError("Age can not be 0 or less.")
+#     return 10 / age
+
+
+# try:
+#     calculate_xfactor(-10)
+# except ValueError as error:
+#     print(error)
+
+# Cost of raising exception
+
+from timeit import timeit
+
+code1 = """
+
 def calculate_xfactor(age):
     if age <= 0:
         raise ValueError("Age can not be 0 or less.")
@@ -56,4 +73,23 @@ def calculate_xfactor(age):
 try:
     calculate_xfactor(-10)
 except ValueError as error:
-    print(error)
+    pass
+
+"""
+
+code2 = """
+
+def calculate_xfactor(age):
+    if age <= 0:
+        return None
+    return 10 / age
+
+
+
+xfactor = calculate_xfactor(-1)
+if xfactor == None:
+    pass
+"""
+
+print("first code = ", timeit(code1, number=10000))
+print("second code = ", timeit(code2, number=10000))
