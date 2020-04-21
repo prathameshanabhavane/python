282300dad0e65d78f629cf776369809231eac822
prathameshanabhavane <pratham291994@gmail.com>
2020-04-21 19:04:26 +0530

    magic method

--- class.py
+++ class.py
@@ -55,18 +55,35 @@
 # another.draw()
 
 # class vs Instance method
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     @classmethod
+#     def zero(cls):
+#         return cls(0, 0)
+
+#     def draw(self):
+#         print(f"Point ({self.x}, { self.y})")
+
+
+# point = Point.zero()
+# point.draw()
+
+
+# magic methods
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    @classmethod
-    def zero(cls):
-        return cls(0, 0)
+    def __str__(self):
+        return f"({self.x}, {self.y})"
 
     def draw(self):
         print(f"Point ({self.x}, { self.y})")
 
 
-point = Point.zero()
-point.draw()
+point = Point(1, 2)
+print(str(point))
