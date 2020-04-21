e75dcb3c4b9b3f578b0367508509cae6e1a583c3
prathameshanabhavane <pratham291994@gmail.com>
2020-04-21 18:35:26 +0530

    constructor

--- class.py
+++ class.py
@@ -2,12 +2,28 @@
 # object:Instance of class
 
 
-class Point:
-    def draw(self):
-        print("draw")
+# class Point:
+#     def draw(self):
+#         print("draw")
 
 
-point = Point()
-print(type(point))
-print(isinstance(point, Point))
+# point = Point()
+# print(type(point))
+# print(isinstance(point, Point))
 # print(isinstance(point, int))
+
+
+# Constructors
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        print(f"Point ({self.x}, { self.y})")
+
+
+point = Point(1, 2)
+print(point.x)
+print(point.y)
+point.draw()
