472b5dbde99686593fb063c80b1745507550430e
prathameshanabhavane <pratham291994@gmail.com>
2020-04-21 19:44:42 +0530

    making custom container

--- class.py
+++ class.py
@@ -112,18 +112,47 @@
 
 # performing arithmatic operations
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
 
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
+#     def __add__(self, other):
+#         return Point(self.x + other.x, self.y + other.y)
 
 
-point = Point(1, 2)
-other = Point(1, 2)
-combined = point + other
+# point = Point(1, 2)
+# other = Point(1, 2)
+# combined = point + other
 
-print(combined.x)
-print(combined.y)
+# print(combined.x)
+# print(combined.y)
+
+# Making custom container
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
+
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+
+    def __getitem__(self, tag):
+        return self.tags.get(tag.lower(), 0)
+
+    def setitem(self, tag, count):
+        self.tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __iter__(self):
+        return iter(self.tags)
+
+
+cloud = TagCloud()
+cloud.add("Python")
+cloud.add("python")
+cloud.add("python")
+cloud.add("python")
+
+print(cloud.tags)
