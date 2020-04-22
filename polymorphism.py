a0c9c5e7488466a62010145c4935df346bd474d9
prathameshanabhavane <pratham291994@gmail.com>
2020-04-22 16:30:45 +0530

    extending built in types

--- polymorphism.py
+++ polymorphism.py
@@ -31,3 +31,48 @@ textbox = TestBox()
 # draw(textbox)
 # draw(ddl)
 draw([ddl, textbox])
+
+
+# Duck Type
+
+class TestBox:
+    def draw(self):
+        print("Text box")
+
+
+class DropDownList:
+    def draw(self):
+        print("Dropdowan list")
+
+
+def draw(controls):
+    for control in controls:
+        control.draw()
+
+
+draw([ddl, textbox])
+
+
+# Extending built in types
+
+class Text(str):
+    def duplicate(self):
+        return self + self
+
+
+text = Text("Python")
+
+# print(text.lower())
+# print(text.duplicate())
+
+
+class TrackableList(list):
+    def append(self, object):
+        print("Append called")
+        super().append(object)
+
+
+list = TrackableList()
+
+list.append("1")
+list.append("Hello")
