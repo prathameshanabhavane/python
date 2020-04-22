5627a3db0484e9a20538da67c6a624ddf15e6fcb
prathameshanabhavane <pratham291994@gmail.com>
2020-04-22 17:44:31 +0530

    packages

--- modules/app.py
+++ modules/app.py
@@ -8,9 +8,28 @@
 # calc_shipping()
 # calc_tax()
 
-import sales
+# import sales
 
-import sys
+# import sys
 
 
-print(sys.path)
+# print(sys.path)
+
+
+# Packages
+
+# import ecommerce.sales
+
+# ecommerce.sales.calc_shipping()
+
+
+# from ecommerce.sales import calc_shipping, calc_tax
+
+# calc_shipping()
+# calc_tax()
+
+
+from ecommerce import sales
+
+sales.calc_shipping()
+sales.calc_tax()
