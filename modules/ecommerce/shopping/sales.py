288d597507ff680f484841f9d4a7657875df20b5
prathameshanabhavane <pratham291994@gmail.com>
2020-04-22 17:55:42 +0530

    intra package reference

--- modules/ecommerce/shopping/sales.py
+++ modules/ecommerce/shopping/sales.py
@@ -1,3 +1,9 @@
+# absolute import statement this is by pep 8 mostly used
+from ecommerce.customer import contact
+
+# from ..customer import contact  # relative import statement (optional)
+
+contact.contact_customer()
 
 
 def calc_shipping():
