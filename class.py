43d76030f539640da677ea6d3ffb1a4e51c8f9e2
prathameshanabhavane <pratham291994@gmail.com>
2020-04-22 12:54:04 +0530

    properties

--- class.py
+++ class.py
@@ -161,3 +161,23 @@ cloud.add("python")
 print(cloud["PYTHON"])
 print(cloud.__dict__)
 print(cloud._TagCloud__tags)
+
+
+# Priperties
+class Product:
+    def __init__(self, price):
+        self.price = price
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price can not be negative")
+        self.__price = value
+
+
+product = Product(-50)
+print(product.price)
