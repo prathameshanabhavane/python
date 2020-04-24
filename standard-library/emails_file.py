455c2d2f13f1a5dd78714d7087f78fed89c9471f
prathameshanabhavane <pratham291994@gmail.com>
2020-04-24 19:01:28 +0530

    email template

--- standard-library/emails_file.py
+++ standard-library/emails_file.py
@@ -1,16 +1,21 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+from string import Template
 from pathlib import Path
 from smtplib import SMTP
 
 # print(Path("unnamed.png"))
 
+template = Template(Path("template.html").read_text())
+
 message = MIMEMultipart()
 message["from"] = "Jhon Doe"
 message["to"] = "test@gmail.com"
 message["subject"] = "This is test"
-message.attach(MIMEText("Body"))
+body = template.substitute({"name": "Jhon"})
+# body = template.substitute(name="Raj")
+message.attach(MIMEText(body, "html"))
 message.attach(MIMEImage(Path("unnamed.jpg").read_bytes()))
 
 
